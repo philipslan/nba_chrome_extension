55891aaae77da54fa210fe543b5123ef3aed680a
Philip Lan <philiplan2017@u.northwestern.edu>
2016-12-19 18:55:26 -0500

    got rid of logic inside html and added overtime in box scores

--- popup.js
+++ popup.js
@@ -6,10 +6,38 @@
             var date = (new Date()).toLocaleDateString("en-US").split("/");
             var dateString = date[2] + date[0] + date[1];
             $http.get(`http://data.nba.net/data/10s/prod/v1/${dateString}/scoreboard.json`).success(function (data) {
-                $scope.games = data.games;
-                $scope.games.forEach(function (game) {
+                $scope.games = data.games.map(function (game) {
+                    var clock = null;
+                    if (game.hTeam.linescore.length == 0) {
+                        clock = "--";
+                    } else if (game.period.isEndOfPeriod && game.period.isHalftime) {
+                        clock = "Half Time";
+                    } else if (game.clock.length || game.period.isEndOfPeriod) {
+                        clock = `Quarter ${game.period.current} - ${game.clock}`;
+                    } else {
+                        clock = "End of Game";
+                    }
+                    // Checking OTs
+                    var vScores = [];
+                    var hScores = [];
+                    for (var i = 0; i < game.vTeam.linescore.length; i++){
+                        if (i > 4) {
+                            vScores[4] += parseInt(game.vTeam.linescore[i].score);
+                            hScores[4] += parseInt(game.hTeam.linescore[i].score);
+                        } else {
+                            vScores.push(parseInt(game.vTeam.linescore[i].score));
+                            hScores.push(parseInt(game.hTeam.linescore[i].score));
+                        }
+                    }
+                    game.vTeam.linescore = vScores;
+                    game.hTeam.linescore = hScores;
                     game.vTeam.img = `http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${game.vTeam.triCode}.svg`;
                     game.hTeam.img = `http://i.cdn.turner.com/nba/nba/assets/logos/teams/secondary/web/${game.hTeam.triCode}.svg`;
+                    return {
+                        vTeam: game.vTeam,
+                        hTeam: game.hTeam,
+                        clock: clock
+                    }
                 });
             });
         };
